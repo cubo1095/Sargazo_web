93334246c5314cb238fe4c7f41da4e3a773beb6f
cubo1095 <jesuher1095@gmail.com>
2021-06-29 23:57:22 -0500

    update

--- pagweb_s/static/js/sargazo.js
+++ pagweb_s/static/js/sargazo.js
@@ -2,54 +2,51 @@ $(window).on("load",sargazo);
 
 function sargazo(){
     var con= 1;
-    sarfe='<input type="checkbox" id="sargazo_31" checked />'+
+    /*sarfe='<input type="checkbox" id="sargazo_31" checked />'+
     '<label for="check1">Sargazo 31/05/2021</label><br>'+
     '<input type="checkbox" id="sargazo_26" checked />'+
     '<label for="check1">Sargazo 26/05/2021</label><br>'+
     '<input type="checkbox" id="sargazo_05"  checked/>'+
-    '<label for="check1">Sargazo 05/06/2021</label><br></br>'
+    '<label for="check1">Sargazo 05/06/2021</label><br></br>'*/
     
     
-    $('#sargazo').click(function(){
-        if(con==1){
-            $('#sargazofecha').html(sarfe);
-            $('#sargazo_26').click(function(){
-                if($(this).is(':checked')){
-                    map.addLayer(wmsLayersargazo_26);
-                } else {
-                    map.removeLayer(wmsLayersargazo_26);
-                }
-            });
-            $('#sargazo_31').click(function(){
-                if($(this).is(':checked')){
-                    map.addLayer(wmsLayersargazo_31);
-                } else {
-                    map.removeLayer(wmsLayersargazo_31);
-                }
-            });
-            $('#sargazo_05').click(function(){
-                if($(this).is(':checked')){
-                    map.addLayer(wmsLayersargazo_05);
-                } else {
-                    map.removeLayer(wmsLayersargazo_05);
-                }
-            });
-            con= 0;
-        }else{
-            con=1;
-            $('#sargazofecha').empty(sarfe);
+    $('#sargazolight').click(function(){
+        var imgsargazo = "static/img/Simbo/sargazo.png";
+        if($(this).is(':checked')){
+            map.addLayer(wmsLayersargazo);
+            $('#imagen').append('<img src="'+imgsargazo+'" class="imgsar"/>');
+        } else {
+            map.removeLayer(wmsLayersargazo);
+            $('img').remove('.imgsar');
         }
-    })
-      
+    });
+    
+    $('#sargazo_d').click(function(){
+        var imgsargazod = "static/img/Simbo/Sargazo_dis.png";
+        if($(this).is(':checked')){
+            map.addLayer(wmsLayersargazod);
+            $('#imagen').append('<img src="'+imgsargazod+'" class="imgsard"/>');
+        } else {
+            map.removeLayer(wmsLayersargazod);
+            $('img').remove('.imgsard');
+        }
+    });
+
+    $('#estudio').click(function(){
+        // var imgsargazo = "static/img/Simbo/sargazo.png";
+        if($(this).is(':checked')){
+            map.addLayer(wmsLayerzona);
+            // $('#imagen').append('<img src="'+imgsargazo+'" class="imgsar"/>');
+        } else {
+            map.removeLayer(wmsLayerzona);
+            // $('img').remove('.imgsar');
+        }
+    });
     
     $('#sargazolight').click(function(){
         var imgsar0506 =  "static/img/Simbo/sargazo0506.png";
-        var imgsar2605 =  "static/img/Simbo/sargazo2605.png";
-        var imgsar3105 =  "static/img/Simbo/sargazo3105.png";
-
+        
         if($(this).is(':checked')){
-            $("#sargazo_31").prop("checked", true);
-            $("#sargazo_26").prop("checked", true);
             $("#sargazo_05").prop("checked", true);
             map.addLayer(wmsLayersargazo_31);
             map.addLayer(wmsLayersargazo_26);
--- pagweb_s/static/js/slideimg.js
+++ pagweb_s/static/js/slideimg.js
@@ -3,7 +3,7 @@ $(document).ready(heightsimbo);
 function heightsimbo(){
 
     switch(true){
-        case $('#fire').is(':checked') && $('#sentineltc').is(':checked'):
+        case $('#fire').is(':checked') && $('#sentineltc').is(':checked') && $('#sargazo_d').is(':checked'):
             $('.Simbologia').css('height','50%');
             break;
 
@@ -15,6 +15,9 @@ function heightsimbo(){
             $('.Simbologia').css('height','50%');
             break;
 
+        case $('#sargazo_d').is(':checked'):
+            $('.Simbologia').css('height','50%');
+            break;
         default:
             $('.Simbologia').css('height','20%');
     }
