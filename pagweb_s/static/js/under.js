f6ee6614731d6f0906d1befedfab6e94d181cb70
cubo1095 <jesuher1095@gmail.com>
2021-08-04 01:44:33 -0500

    update last

--- pagweb_s/static/js/under.js
+++ pagweb_s/static/js/under.js
@@ -6,23 +6,23 @@ function under(){
   
     $('#fig').click(function(){
         if(contador == 1){
-            $('.Capas').animate({right: '0%'});
-            $("#fig").css({"box-shadow": "0px 0px 15px #e766f8", "border-radius": "5px", "border": "2px solid", "border-color": "#e766f8", "background": "white"});
-            $('#fig').animate({right: '265px'});
-            $('#fig1').animate({right: '265px'});
-            $('#fig2').animate({right: '265px'});
-            $('.ol-zoom').animate({right: '265px'});
-            $('#mouse-position').animate({right: '265px'})
+            $('.conten').animate({left: '-100%'});
+            // $("#fig").css({"box-shadow": "0px 0px 15px #e766f8", "border-radius": "5px", "border": "2px solid", "border-color": "#e766f8", "background": "white"});
+            // $('#fig').animate({right: '265px'});
+            // $('#fig1').animate({right: '265px'});
+            // $('#fig2').animate({right: '265px'});
+            // // $('.ol-zoom').animate({right: '265px'});
+            // $('#mouse-position').animate({right: '265px'})
             contador = 0;
         } else {
             contador = 1;
-            $('.Capas').animate({right: '-100%'});
-            $('#fig').animate({right: '3%'});
-            $("#fig").css({"box-shadow": "none", "border": "2px solid", "border-color":"black", "border-radius": "5px"});
-            $('#fig1').animate({right: '3%'});
-            $('#fig2').animate({right: '3%'});
-            $('.ol-zoom').animate({right: '3%'});
-            $('#mouse-position').animate({right: '5%'})
+            $('.conten').animate({left: '0'});
+            // $('#fig').animate({right: '3%'});
+            // $("#fig").css({"box-shadow": "none", "border": "2px solid", "border-color":"black", "border-radius": "5px"});
+            // $('#fig1').animate({right: '3%'});
+            // $('#fig2').animate({right: '3%'});
+            // $('.ol-zoom').animate({right: '3%'});
+            // $('#mouse-position').animate({right: '5%'})
         }
 
     });
