527260c9f1f234eb8180f88796f15321139720d0
cubo1095 <jesuher1095@gmail.com>
2021-09-07 19:38:00 -0500

    update

--- pagweb_s/static/js/under.js
+++ pagweb_s/static/js/under.js
@@ -17,8 +17,73 @@ function under(){
         $('#fig').animate({left: '0%'},1500);
     });
     document.getElementById("defaultOpen").click();
+
+    //Desplazamiento de directorios Hycom
+    con = 1;
+    $('#hycomser').click(function(){
+        if(con == 1){
+            $('#hycomcont').html('<input type="checkbox" id="hycomtemp"/><label for="check1">   Temperatura del mar</label><br>'
+                +'<input type="checkbox" id="hycomvelo"/><label for="check1">   Velocidad del mar</label><br>');
+                
+            $('#hycomtemp').click(function(){
+                if($(this).is(':checked')){
+                    map.addLayer(wmsLayerHYCOMtem);
+                    // $('#imagen').append('<img src="'+imgest+'" class="imgest"/>');
+                } else {
+                    map.removeLayer(wmsLayerHYCOMtem);
+                    // $('img').remove('.imgest');
+                }
+            });
+
+            $('#hycomvelo').click(function(){
+                // var imgest =  "static/img/Simbo/estados.png";
+                if($(this).is(':checked')){
+                    map.addLayer(wmsLayerHYCOMvel);
+                    alert('hola')
+                    // $('#imagen').append('<img src="'+imgest+'" class="imgest"/>');
+                } else {
+                    map.removeLayer(wmsLayerHYCOMvel);
+                    // $('img').remove('.imgest');
+                }
+            });
+
+            con = 0;
+        }else{
+            $('#hycomcont').html('');
+            con = 1;
+        }
+    });
+    
+    //Layer Hycomtemperatura
+    var wmsSourceHYCOMtem = new ol.source.TileWMS({
+        visible: true,
+        url: 'https://wms.hycom.org/thredds/wms/GLBy0.08/latest?crs=EPSG:4326&dpiMode=7&format=image/png&layers=water_temp&styles',
         
+        serverType: 'HYCOM'
+        });
+
+    var wmsLayerHYCOMtem = new  ol.layer.Tile({
+        source: wmsSourceHYCOMtem,
+        zIndex: 0.5
+    });
+    
+
+    //Layer Hycom velocidad
+    var wmsSourceHYCOMvel = new ol.source.TileWMS({
+        visible: true,
+        url: 'https://wms.hycom.org/thredds/wms/GLBy0.08/latest?crs=EPSG:4326&dpiMode=7&format=image/png&layers=sea_water_velocity&styles',
+        
+        serverType: 'HYCOM'
+        });
+
+    var wmsLayerHYCOMvel = new  ol.layer.Tile({
+        source: wmsSourceHYCOMvel,
+        zIndex: 0.5
+    });
+    
 };
+
+
 
 //función de pestañas
 function openc(evt, act) {
